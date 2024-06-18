1cafb264292670d7e2128ef266cdec7492d01645
Kulkarni <vkulkarni46@ice-login2.icehammer.gtri.org>
2024-06-18 15:04:39 -0400

    Add command line arguments

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
+import argparse
 from pipeline import Pipeline
-import primitives
 
-NUM_GENERATIONS = 10
 
-GaPipeline = Pipeline()
-GaPipeline.clear_outputs()
+parser = argparse.ArgumentParser()
+    
+parser.add_argument('-o', '--outputs', type=str, required=True, help='The output directory')
+parser.add_argument('-f', '--force', action='store_true', help='Force overwrite if output directory exists')
+parser.add_argument('-n', '--num_generations', type=int, required=True, help='The number of generations to run the evolution for')
+parser.add_argument('-c', '--clean', action='store_true', help='Cleans output directory of non pareto optimal individuals')
+
+args = parser.parse_args()
+
+output_dir = args.outputs
+force_flag = args.force
+num_gen = args.num_generations
+clean = args.clean
+
+GaPipeline = Pipeline(output_dir, force_flag, clean)
 GaPipeline.init_pop()
-for i in range (NUM_GENERATIONS):
+for i in range (num_gen):
     print(f'---------- Generation {GaPipeline.gen_count} ----------')
     GaPipeline.evaluate_gen()
     selected_parents = GaPipeline.select_parents()
@@ -16,3 +28,6 @@ for i in range (NUM_GENERATIONS):
     unsustainable_pop = GaPipeline.overpopulate(selected_parents + elites) # returns dict {hash: genome}
     GaPipeline.downselect(unsustainable_pop) # population is replaced by a completely new one
     GaPipeline.step_gen()
+
+print('====================')
+print(f'total genome fails: {GaPipeline.num_genome_fails}/{num_gen*GaPipeline.population_size}')
