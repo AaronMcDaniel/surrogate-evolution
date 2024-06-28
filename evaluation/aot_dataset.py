a34ded352184fdf4117e14b0edeb554c60d7dcfa
Thakur <tthakur9@ice-login2.icehammer.gtri.org>
2024-06-28 15:32:42 -0400

    more evaluation testing, not fully functional

--- evaluation/aot_dataset.py
+++ evaluation/aot_dataset.py
@@ -5,46 +5,71 @@ from torch.utils.data import Dataset
 import cv2
 from torch.utils.data import DataLoader
 import torch
-
+import json
+import time
 
 class AOTDataset(Dataset):
-    def __init__(self, mode):
-        if mode == 'train':
-            self.pickle_path = '/gv1/projects/GRIP_Precog_Opt/data_loading/airborne-detection-starter-kit-master/data/part1/pickles/labels/part1_NOSTRING_TENSOR_labels.pkl'
-            self.image_folder = '/gv1/projects/GRIP_Precog_Opt/data_loading/airborne-detection-starter-kit-master/data/part1/'
-        elif mode == 'val':
-            self.pickle_path = '/gv1/projects/GRIP_Precog_Opt/data_loading/airborne-detection-starter-kit-master/data/part2/pickles/labels/part2_NOSTRING_TENSOR_labels.pkl'
-            self.image_folder = '/gv1/projects/GRIP_Precog_Opt/data_loading/airborne-detection-starter-kit-master/data/part2/'
-        elif mode == 'test':
-            self.pickle_path = '/gv1/projects/GRIP_Precog_Opt/data_loading/airborne-detection-starter-kit-master/data/part3/pickles/labels/part3_NOSTRING_TENSOR_labels.pkl'
-            self.image_folder = '/gv1/projects/GRIP_Precog_Opt/data_loading/airborne-detection-starter-kit-master/data/part3/'
+    def __init__(self, mode, string=None):
+        
+        if string == None:
+            if mode == 'train':
+                self.pickle_path = '/gv1/projects/GRIP_Precog_Opt/data_loading/airborne-detection-starter-kit-master/data/part1/pickles/labels/part1_NOSTRING_TENSOR_labels.pkl'
+                self.image_folder = '/gv1/projects/GRIP_Precog_Opt/data_loading/airborne-detection-starter-kit-master/data/part1/'
+            elif mode == 'val':
+                self.pickle_path = '/gv1/projects/GRIP_Precog_Opt/data_loading/airborne-detection-starter-kit-master/data/part2/pickles/labels/part2_NOSTRING_TENSOR_labels.pkl'
+                self.image_folder = '/gv1/projects/GRIP_Precog_Opt/data_loading/airborne-detection-starter-kit-master/data/part2/'
+            elif mode == 'test':
+                self.pickle_path = '/gv1/projects/GRIP_Precog_Opt/data_loading/airborne-detection-starter-kit-master/data/part3/pickles/labels/part3_NOSTRING_TENSOR_labels.pkl'
+                self.image_folder = '/gv1/projects/GRIP_Precog_Opt/data_loading/airborne-detection-starter-kit-master/data/part3/'
+        else:
+            if mode == 'train':
+                self.pickle_path = '/gv1/projects/GRIP_Precog_Opt/data_loading/airborne-detection-starter-kit-master/data/part1/pickles/labels/part1_STRING_TENSOR_labels.pkl'
+                self.image_folder = '/gv1/projects/GRIP_Precog_Opt/data_loading/airborne-detection-starter-kit-master/data/part1/'
+            elif mode == 'val':
+                self.pickle_path = '/gv1/projects/GRIP_Precog_Opt/data_loading/airborne-detection-starter-kit-master/data/part2/pickles/labels/part2_STRING_TENSOR_labels.pkl'
+                self.image_folder = '/gv1/projects/GRIP_Precog_Opt/data_loading/airborne-detection-starter-kit-master/data/part2/'
+            elif mode == 'test':
+                self.pickle_path = '/gv1/projects/GRIP_Precog_Opt/data_loading/airborne-detection-starter-kit-master/data/part3/pickles/labels/part3_STRING_TENSOR_labels.pkl'
+                self.image_folder = '/gv1/projects/GRIP_Precog_Opt/data_loading/airborne-detection-starter-kit-master/data/part3/'
 
         with open(self.pickle_path, 'rb') as f:
             self.labels = pickle.load(f)
         print(mode, 'labels loaded')
 
+        self.string = string
         self.class_mapping_all = {'Airborne1': 0, 'Airborne2': 1, 'Airborne3': 2, 'Airborne4': 3, 'Airborne5': 4, 'Airborne6': 5, 'Airborne7': 6, 'Airborne9': 7, 'Airborne10': 8, 'Airborne11': 9, 'Airborne12': 10, 'Airborne14': 11, 'Airborne16': 12, 'Airborne17': 13, 'Airborne18': 14, 'Airplane1': 15, 'Airplane2': 16, 'Airplane3': 17, 'Airplane4': 18, 'Airplane5': 19, 'Airplane6': 20, 'Airplane7': 21, 'Airplane8': 22, 'Airplane10': 23, 'Bird1': 24, 'Bird2': 25, 'Bird3': 26, 'Bird4': 27, 'Bird5': 28, 'Bird6': 29, 'Bird7': 30, 'Bird8': 31, 'Bird9': 32, 'Bird10': 33, 'Bird15': 34, 'Bird23': 35, 'Drone1': 36, 'Flock1': 37, 'Flock2': 38, 'Flock3': 39, 'Helicopter1': 40, 'Helicopter2': 41, 'Helicopter3': 42}
         self.class_mapping = {'Airborne' : 0, 'Airplane' : 1, 'Bird' : 2, 'Drone' : 3, 'Flock' : 4, 'Helicopter' : 5}
-        #self.eval_labels = self.eval_batch(self.labels)
-        
+        self.last = 0
 
     def __len__(self):
         return len(self.labels)
 
     def __getitem__(self, idx):
         label = self.labels[idx]
-        #evaluated = eval_label(label)
-        #test_label = {}
-        #test_label['boxes'] = label['boxes'].numpy()
+        if self.string is not None:
+            label = eval_label(label)
         image_path = label['path']
+        try:
+            image = cv2.imread(self.image_folder + image_path)
+            image = np.array(image)
+        except:
+            label = self.labels[self.last]
+            if self.string is not None:
+                label = eval_label(label)
+            image_path = label['path']
+            image = cv2.imread(self.image_folder + image_path)
         image = cv2.imread(self.image_folder + image_path)
-        image = np.array(image)
+        image = np.array(image, dtype=np.int32)
         image = torch.from_numpy(image)
+        self.last = idx
         return image, label
 
     def find_image(self, frame_id, flight_id):
-        #labels = eval_batch(self.labels)
-        for label in self.labels:
+        if self.string is not None:
+            labels = eval_batch(self.labels)
+        else:
+            labels = self.labels
+        for label in labels:
             if label['flight_id'] == flight_id and label['frame_id'] == frame_id:
                 output = self.image_folder + label['path']
                 return output
@@ -55,52 +80,18 @@ def my_collate(batch):
     for i, item in enumerate(batch):
         images.append(batch[i][0])
         labels.append(batch[i][1])
-  
     return [images, labels]
 
-# class AOTDataset(Dataset):
-#     def __init__(self, mode):
-#         if mode == 'train':
-#             self.pickle_path = '/gv1/projects/GRIP_Precog_Opt/data_loading/airborne-detection-starter-kit-master/data/part1/pickles/labels/part1_STRING_TENSOR_labels.pkl'
-#             self.image_folder = '/gv1/projects/GRIP_Precog_Opt/data_loading/airborne-detection-starter-kit-master/data/part1/'
-#         elif mode == 'val':
-#             self.pickle_path = '/gv1/projects/GRIP_Precog_Opt/data_loading/airborne-detection-starter-kit-master/data/part2/pickles/labels/part2_STRING_TENSOR_labels.pkl'
-#             self.image_folder = '/gv1/projects/GRIP_Precog_Opt/data_loading/airborne-detection-starter-kit-master/data/part2/'
-#         elif mode == 'test':
-#             self.pickle_path = '/gv1/projects/GRIP_Precog_Opt/data_loading/airborne-detection-starter-kit-master/data/part3/pickles/labels/part3_STRING_TENSOR_labels.pkl'
-#             self.image_folder = '/gv1/projects/GRIP_Precog_Opt/data_loading/airborne-detection-starter-kit-master/data/part3/'
+def eval_label(label):
+    nan = np.nan
+    def array(x):
+        return np.array(x)
+    def tensor(x, dtype=torch.int64):
+        return torch.tensor(x)
+    return eval(label) 
 
-#         with open(self.pickle_path, 'rb') as f:
-#             self.labels = pickle.load(f)
-#         print('labels loaded!')
-
-#         self.class_mapping_all = {'Airborne1': 0, 'Airborne2': 1, 'Airborne3': 2, 'Airborne4': 3, 'Airborne5': 4, 'Airborne6': 5, 'Airborne7': 6, 'Airborne9': 7, 'Airborne10': 8, 'Airborne11': 9, 'Airborne12': 10, 'Airborne14': 11, 'Airborne16': 12, 'Airborne17': 13, 'Airborne18': 14, 'Airplane1': 15, 'Airplane2': 16, 'Airplane3': 17, 'Airplane4': 18, 'Airplane5': 19, 'Airplane6': 20, 'Airplane7': 21, 'Airplane8': 22, 'Airplane10': 23, 'Bird1': 24, 'Bird2': 25, 'Bird3': 26, 'Bird4': 27, 'Bird5': 28, 'Bird6': 29, 'Bird7': 30, 'Bird8': 31, 'Bird9': 32, 'Bird10': 33, 'Bird15': 34, 'Bird23': 35, 'Drone1': 36, 'Flock1': 37, 'Flock2': 38, 'Flock3': 39, 'Helicopter1': 40, 'Helicopter2': 41, 'Helicopter3': 42}
-#         self.class_mapping = {'Airborne' : 0, 'Airplane' : 1, 'Bird' : 2, 'Drone' : 3, 'Flock' : 4, 'Helicopter' : 5}
-#         #self.eval_labels = self.eval_batch(self.labels)
-        
-
-#     def __len__(self):
-#         return len(self.labels)
-
-#     def __getitem__(self, idx):
-#         label = self.labels[idx]
-#         evaluated = eval_label(label)
-#         image_path = evaluated['path']
-#         image = cv2.imread(self.image_folder + image_path)
-#         image = np.array(image)
-#         image = torch.from_numpy(image)
-#         return image, label
-
-# def eval_label(label):
-#     nan = np.nan
-#     def array(x):
-#         return np.array(x)
-#     def tensor(x, dtype=torch.int64):
-#         return torch.tensor(x)
-#     return eval(label) #, {'nan': np.nan, 'array': self.array})
-
-# def eval_batch(batch):
-#     batch = list(batch)
-#     for i, label in enumerate(batch):
-#         batch[i] = eval_label(batch[i])
-#     return batch
+def eval_batch(batch):
+    batch = list(batch)
+    for i, label in enumerate(batch):
+        batch[i] = eval_label(batch[i])
+    return batch
--- evaluation/sample_model.py
+++ evaluation/sample_model.py
@@ -1,31 +1,61 @@
 import torch
 import torchvision
-from torchvision.models.detection import FasterRCNN
-from torchvision.models.detection.rpn import AnchorGenerator
+from custom_detectors.custom_rcnn import CustomFasterRCNN
+from custom_detectors.custom_fcos import CustomFCOS
+from custom_detectors.custom_retinanet import CustomRetinaNet
+from torchvision.models.detection.rpn import AnchorGenerator as RCNNAnchorGenerator
+from torchvision.models.detection.anchor_utils import AnchorGenerator
+
 from model_summary import detection_model_summary
 
-def get_model_dict(num_loss_components):
+
+def get_model_dict(model_type, num_classes, num_loss_components):
     # Load a pretrained ConvNeXt backbone
     backbone = torchvision.models.convnext_base(weights="DEFAULT").features
     backbone.out_channels = 1024  # ConvNeXt's output feature maps channels
+    model = backbone
+    if model_type == 'FasterRCNN':
+        anchor_generator = RCNNAnchorGenerator(
+            sizes=((8, 16, 32, 64, 128, 256),),
+            aspect_ratios=((0.5, 1.0, 2.0),)
+        )
+        roi_pooler = torchvision.ops.MultiScaleRoIAlign(
+            featmap_names=['0'],
+            output_size=21,
+            sampling_ratio=4
+        )
+        model = CustomFasterRCNN(
+            model,
+            num_classes=num_classes,
+            rpn_anchor_generator=anchor_generator,
+            box_roi_pool=roi_pooler,
+            box_score_thresh=0.05
+        )
+    elif model_type == 'FCOS':
+        anchor_generator = AnchorGenerator(
+            sizes=((8,), (16,), (32,), (64,), (128,), (256,)),
+            aspect_ratios=((1.0,),)
+        )
+        model = CustomFCOS(
+            model,
+            num_classes=num_classes,
+            anchor_generator=anchor_generator,
+            score_thresh=0.05
+        )
+    elif model_type == 'RetinaNet':
+        anchor_generator = AnchorGenerator(
+            sizes=((8, 16, 32, 64, 128, 256),),
+            aspect_ratios=((0.5, 1.0, 2.0),)
+        )
+        model = CustomRetinaNet(
+            model,
+            num_classes=num_classes,
+            anchor_generator=anchor_generator,
+            score_thresh=0.05
+        )
 
-    anchor_generator = AnchorGenerator(
-                sizes=((16, 32, 64, 128, 256, 512),),
-                aspect_ratios=((0.5, 1.0, 2.0),)
-            )
-    roi_pooler = torchvision.ops.MultiScaleRoIAlign(
-        featmap_names=['0'],
-        output_size=21,
-        sampling_ratio=4
-    )
-
-    # Put the pieces together to create a Faster R-CNN model
-    model = FasterRCNN(
-        backbone,
-        num_classes=7,  # Including background
-        rpn_anchor_generator=anchor_generator,
-        box_roi_pool=roi_pooler
-    )
+    else:
+        raise ValueError('model_type bust be one of the following: "FasterRCNN", "FCOS", "RetinaNet"')
 
     weights = torch.rand_like(torch.ones(num_loss_components))
     weights /= weights.sum()
