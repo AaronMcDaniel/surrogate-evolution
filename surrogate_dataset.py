8ae30e071e0d2c6bf83154395c6ab76ad199eae6
Kulkarni <vkulkarni46@ice-login2.icehammer.gtri.org>
2024-07-09 18:12:19 -0400

    Add train-val split functionality

--- surrogate_dataset.py
+++ surrogate_dataset.py
@@ -11,6 +11,7 @@ Steps:
 import argparse
 import math
 import os
+import random
 
 import pandas as pd
 import toml
@@ -22,6 +23,8 @@ parser.add_argument('-w', '--working', required=False, default='/gv1/projects/GR
 parser.add_argument('-o', '--outdir', required=False, default='surrogate_dataset')
 parser.add_argument('-m', '--metrics', required=False, default='uw_val_epoch_loss,iou_loss,giou_loss,diou_loss,ciou_loss,center_loss,size_loss,obj_loss,precision,recall,f1_score,average_precision')
 parser.add_argument('-x', '--exclude', required=False, default='')
+parser.add_argument('-v', '--valratio', required=False, default=0.3)
+parser.add_argument('-s', '--seed', required=False, default=0)
 args = parser.parse_args()
 infile = args.infile
 working_dir = args.working
@@ -30,6 +33,10 @@ metric_headings = args.metrics
 metric_headings = metric_headings.split(',')
 excluded_gens = args.exclude
 excluded_gens = [int(gen) for gen in excluded_gens.split(',')]
+val_ratio = args.valratio
+seed = args.seed
+
+local_random = random.Random(seed)
 
 
 configs = toml.load(os.path.join(working_dir, "conf.toml"))
@@ -43,6 +50,7 @@ out_data = pd.DataFrame(columns=['genome', 'epoch_num']+metric_headings)
 data = pd.read_csv(infile)
 data = data.to_dict('records')
 
+all_genome_info = []
 
 for line in data:
     genome_hash = line['hash']
@@ -55,12 +63,16 @@ for line in data:
     metrics = pd.read_csv(os.path.join(working_dir, f'generation_{gen}', genome_hash, 'metrics.csv'))
     metrics = metrics.to_dict('records')
 
+    genome_info = []
+
     for metric_row in metrics:
         to_add = {}
         to_add['genome'] = genome
 
         if 'epoch_num' not in metric_row:
             for i in range(num_epochs):
+                to_add = {}
+                to_add['genome'] = genome
                 to_add['epoch_num'] = i + 1
                 for heading in metric_headings:
                     if heading in MAX_METRICS:
@@ -68,6 +80,7 @@ for line in data:
                     else:
                         to_add[heading] = 1000000
                 out_data.loc[len(out_data)] = to_add
+                genome_info.append(to_add)
             continue
 
         to_add['epoch_num'] = metric_row['epoch_num']
@@ -80,8 +93,28 @@ for line in data:
             else:
                 to_add[heading] = metric_row[heading]
         out_data.loc[len(out_data)] = to_add
+        genome_info.append(to_add)
+    
+    all_genome_info.append(genome_info)
 
-output_filename = os.path.join(outdir, 'complete_dataset.csv')
-os.makedirs(os.path.dirname(output_filename), exist_ok=True)
-out_data.to_csv(output_filename, index=False)
-        
+
+val_size = int(val_ratio*len(all_genome_info))
+shuffled_data = all_genome_info[:]
+local_random.shuffle(shuffled_data)
+val_genome_info = shuffled_data[:val_size]
+train_genome_info = shuffled_data[val_size:]
+val_genomes = [epoch for genome in val_genome_info for epoch in genome]
+train_genomes = [epoch for genome in train_genome_info for epoch in genome]
+local_random.shuffle(train_genomes)
+local_random.shuffle(val_genomes)
+
+train_set = pd.DataFrame(train_genomes)
+val_set = pd.DataFrame(val_genomes)
+
+complete_output_filename = os.path.join(outdir, 'complete_dataset.csv')
+train_output_filename = os.path.join(outdir, 'train_dataset.csv')
+val_output_filename = os.path.join(outdir, 'val_dataset.csv')
+os.makedirs(os.path.dirname(complete_output_filename), exist_ok=True)
+out_data.to_csv(complete_output_filename, index=False)
+train_set.to_csv(train_output_filename, index=False)
+val_set.to_csv(val_output_filename, index=False)
