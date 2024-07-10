4c364b5944592bc3d7ad5199937d72073207aaa3
Thakur <tthakur9@ice-login2.icehammer.gtri.org>
2024-07-10 10:20:56 -0400

    Fixed mismatch device issue for val_batch_loss calculation in val loop.

--- eval.py
+++ eval.py
@@ -372,7 +372,7 @@ def val_one_epoch(model, device, val_loader, iou_thresh, conf_thresh, loss_weigh
             with autocast():
                 outputs = model(images, targets)
 
-            val_batch_loss = torch.zeros(1, dtype=torch.float32)
+            val_batch_loss = torch.zeros(1, dtype=torch.float32, device=device)
             for j, image in enumerate(images):
                 pred_boxes, true_boxes, flight_id, frame_id = process_preds_truths(targets[j], outputs[j])
                 # NOTE pred boxes are normalized, in xywh format, and have scores, and true boxes are nomalized and in xywh format
