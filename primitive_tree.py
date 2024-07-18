e1aa0bc3c3defca24fb1ff8bfb4d3ed0f6865401
Thakur <tthakur9@ice-login2.icehammer.gtri.org>
2024-07-18 10:48:52 -0400

    resolves circular import error

---
+++ primitive_tree.py
@@ -0,0 +1,36 @@
+from collections import deque
+import re
+from deap import gp  
+
+class CustomPrimitiveTree(gp.PrimitiveTree):
+    @classmethod
+    def from_string(cls, string, pset):
+        tokens = re.split("[ \t\n\r\f\v(),]", string)
+        expr = []
+        ret_types = deque()
+        for token in tokens:
+            if token == '':
+                continue
+            if len(ret_types) != 0:
+                type_ = ret_types.popleft()
+            else:
+                type_ = None
+
+            if token in pset.mapping:
+                primitive = pset.mapping[token]
+
+                expr.append(primitive)
+                if isinstance(primitive, gp.Primitive):
+                    ret_types.extendleft(reversed(primitive.args))
+            else:
+                try:
+                    token = eval(token)
+                except NameError:
+                    raise TypeError("Unable to evaluate terminal: {}.".format(token))
+
+                if type_ is None:
+                    type_ = type(token)
+
+                expr.append(gp.Terminal(token, False, type_))
+        return cls(expr)
+        
