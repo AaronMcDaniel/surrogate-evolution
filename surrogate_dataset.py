Fix surrogate scaling

surrogate_dataset.py
@@ -34,12 +34,12 @@ class SurrogateDataset(Dataset):
         self.min_metrics = torch.ones((1, len(metrics_subset))) * -300.0
         
         # # # standardize genome/metrics data dist if train mode
-        # if mode == 'train':
-        #     # self.metrics = self.metrics_scaler.fit_transform(self.metrics)
-        #     self.genomes = self.genomes_scaler.fit_transform(self.genomes)
-        # if mode == 'val':
-        #     # self.metrics = self.metrics_scaler.transform(self.metrics)
-        #     self.genomes = self.genomes_scaler.transform(self.genomes)
+        if mode == 'train':
+            # self.metrics = self.metrics_scaler.fit_transform(self.metrics)
+            self.genomes = self.genomes_scaler.fit_transform(self.genomes)
+        if mode == 'val':
+            # self.metrics = self.metrics_scaler.transform(self.metrics)
+            self.genomes = self.genomes_scaler.transform(self.genomes)
 
         # # self.max_metrics = torch.tensor(self.metrics_scaler.transform(self.max_metrics), dtype=torch.float32)
         # # self.min_metrics = torch.tensor(self.metrics_scaler.transform(self.min_metrics), dtype=torch.float32)