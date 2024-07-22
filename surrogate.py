c290fbd73f1f67a533c94f7cab0779f6cccacde8
Kulkarni <vkulkarni46@ice-login2.icehammer.gtri.org>
2024-07-22 16:41:19 -0400

    Individuals from file can be filtered on hash, cleanup

--- surrogate.py
+++ surrogate.py
@@ -13,6 +13,7 @@ import torch
 import torch.optim as optim
 from sklearn.preprocessing import StandardScaler
 import surrogate_dataset as sd
+import random
 
 
 def ensure_deap_classes(objectives, codec_config):
@@ -40,23 +41,6 @@ class Surrogate():
         codec_config = configs["codec"]
         model_config = configs["model"]
         self.models = [
-            {
-                'name': 'test',
-                # 'dropout': 0.0,
-                # 'hidden_sizes': [512, 256],
-                # 'optimizer': optim.Adam,
-                # 'lr': 0.0001,
-                # 'scheduler': optim.lr_scheduler.StepLR,
-                # 'metrics_subset': [0, 4, 11],
-                'dropout': 0.2,
-                'hidden_sizes': [2048, 1024, 512],
-                'optimizer': optim.Adam,
-                'lr': 0.1,
-                'scheduler': optim.lr_scheduler.StepLR,
-                'metrics_subset': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
-                'validation_subset': [0, 4, 11],
-                'model': sm.MLP
-            },
             {
                 'name': 'best_overall',
                 # 'dropout': 0.0,
@@ -150,39 +134,42 @@ class Surrogate():
     
     # The calc_pool is a list of deap individuals with calculated fitnesses. The model infers the metrics and 
     # we see the intersection in selections
-    def calc_ensemble_trust(self, model_idxs, genome_scaler, calc_pool):        
+    def calc_ensemble_trust(self, model_idxs, genome_scaler, calc_pool, rand = False):        
         # create copy of calc_pool
         surrogate_pool = copy.deepcopy(calc_pool)
         
-        unique_model_idxs = list(set(model_idxs))
-        unique_inferences = []
-        
-        for model_idx in unique_model_idxs:
-            # get inferences on copy of calc_pool and assign fitness to copy
-            inferences = self.get_surrogate_inferences(model_idx, genome_scaler, surrogate_pool)
-            unique_inferences.append(inferences)
+        if not rand:
+            unique_model_idxs = list(set(model_idxs))
+            unique_inferences = []
             
-            #fitness_idx = model_idxs.index(model_idx)
-        #print('START')
-        #print('info', unique_model_idxs, unique_inferences[0][0], unique_inferences[1][0])
-        constructed_inferences = []
-        #print(len(unique_inferences[0]), len(unique_inferences[1]), len(surrogate_pool))
-        for idx in range(len(surrogate_pool)):
-            fitnesses = []
-            for i, model_idx in enumerate(model_idxs):
-                unique_idx = unique_model_idxs.index(model_idx)
-                if len(unique_inferences[unique_idx][idx]) == 1:
-                    i = 0
-                #print(idx, unique_idx, i, unique_inferences[unique_idx][idx][i])
-                fitnesses.append(unique_inferences[unique_idx][idx][i])
-            #print('fitnesses', fitnesses)
-            constructed_inferences.append(tuple(fitnesses))
-        #print('constructed inferences', constructed_inferences)
-            #for inference in unique_inferences[unique_idx]:
+            for model_idx in unique_model_idxs:
+                # get inferences on copy of calc_pool and assign fitness to copy
+                print(f'    Getting inferences using {self.models[model_idx]['name']}...')
+                inferences = self.get_surrogate_inferences(model_idx, genome_scaler, surrogate_pool)
+                unique_inferences.append(inferences)
+                
+                #fitness_idx = model_idxs.index(model_idx)
+            #print('START')
+            #print('info', unique_model_idxs, unique_inferences[0][0], unique_inferences[1][0])
+            print('    Constructing fitnessess...')
+            constructed_inferences = []
+            #print(len(unique_inferences[0]), len(unique_inferences[1]), len(surrogate_pool))
+            for idx in range(len(surrogate_pool)):
+                fitnesses = []
+                for i, model_idx in enumerate(model_idxs):
+                    unique_idx = unique_model_idxs.index(model_idx)
+                    if len(unique_inferences[unique_idx][idx]) == 1:
+                        i = 0
+                    #print(idx, unique_idx, i, unique_inferences[unique_idx][idx][i])
+                    fitnesses.append(unique_inferences[unique_idx][idx][i])
+                #print('fitnesses', fitnesses)
+                constructed_inferences.append(tuple(fitnesses))
+            #print('constructed inferences', constructed_inferences)
+                #for inference in unique_inferences[unique_idx]:
 
 
-        for i, individual in enumerate(surrogate_pool):
-            individual.fitness.values = constructed_inferences[i]
+            for i, individual in enumerate(surrogate_pool):
+                individual.fitness.values = constructed_inferences[i]
         
         # '''TESTING'''
         # for i in range(len(calc_pool)):
@@ -190,12 +177,16 @@ class Surrogate():
         
         # run trust-calc strategy to select trust_calc_ratio-based number of individuals for both calc_pool and its copy
         # TODO: add other cases of trust_calc_strategy
+        print('    Calculating trust...')
         match self.trust_calc_strategy.lower():
             case 'spea2':
                 self.toolbox.register("select", tools.selSPEA2, k = int(len(calc_pool)*self.trust_calc_ratio))
         
         selected = [self.__get_hash(str(g)) for g in self.toolbox.select(calc_pool)]
-        surrogate_selected = [self.__get_hash(str(g)) for g in self.toolbox.select(surrogate_pool)]
+        if not rand:
+            surrogate_selected = [self.__get_hash(str(g)) for g in self.toolbox.select(surrogate_pool)]
+        else:
+            surrogate_selected = [self.__get_hash(str(g)) for g in random.sample(surrogate_pool, int(len(calc_pool)*self.trust_calc_ratio))]
         
         # check intersection of selected individuals and return
         selected = set(selected)
@@ -263,7 +254,7 @@ class Surrogate():
         model = model(output_size=output_size, **filtered_params).to(self.device)
         
         # Load model weights
-        model.load_state_dict(torch.load(f'/gv1/projects/GRIP_Precog_Opt/surrogates/run_weights/{model_name}.pth', map_location=self.device)) # weights dir is hardcoded rn
+        model.load_state_dict(torch.load(f'/gv1/projects/GRIP_Precog_Opt/surrogates/run_weights/{model_name}.pth', map_location=self.device))
         model.eval()
         
         all_inferences = []
@@ -316,7 +307,7 @@ class Surrogate():
     # with fitness that can be used to either train on or calculate trust with
     # parameter generations tells us what generations to get individuals from. Will use all individuals in a file if unspecified
     # CLIPS TARGET VALUES
-    def get_individuals_from_file(self, filepath, generations=None):
+    def get_individuals_from_file(self, filepath, generations=None, hashes=None):
         # Read the CSV file into a DataFrame
         genomes_df = pd.read_csv(filepath)
 
@@ -327,6 +318,12 @@ class Surrogate():
         # Filter the DataFrame based on generations if provided
         if generations is not None:
             genomes_df = genomes_df[genomes_df['gen'].isin(generations)]
+            
+        # Filter the DataFrame based on hashes if provided
+        if hashes is not None:
+            if 'hash' not in genomes_df.columns:
+                raise ValueError("The DataFrame does not contain a 'hash' column.")
+            genomes_df = genomes_df[genomes_df['hash'].isin(hashes)]
 
         # Convert the DataFrame to a list of DEAP individuals
         genomes = genomes_df['genome'].values  
@@ -355,12 +352,4 @@ class Surrogate():
     
     def __get_hash(self, s):
         return hashlib.shake_256(s.encode()).hexdigest(5)
-
-
-surrogate = Surrogate('conf.toml')
-individuals = surrogate.get_individuals_from_file("/gv1/projects/GRIP_Precog_Opt/unseeded_baseline_evolution/out.csv")
-train_df = pd.read_pickle('surrogate_dataset/train_dataset.pkl')
-train_dataset = sd.SurrogateDataset(train_df, mode='train', metrics_subset=[0, 4, 11])
-genome_scaler = train_dataset.genomes_scaler
-print(surrogate.calc_ensemble_trust([2, 3, 4], genome_scaler, individuals))
      
