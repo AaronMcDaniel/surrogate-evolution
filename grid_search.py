22bc10e5609e2c28b8abc1cab945ec12f259a5e7
Thakur <tthakur9@ice-login2.icehammer.gtri.org>
2024-07-23 11:17:50 -0400

    Grid search for KAN parameters changes

--- grid_search.py
+++ grid_search.py
@@ -48,14 +48,15 @@ def engine(cfg, model_str, param_combo, combo_num):
             
 
 def build_configuration(model_str, device, param_combo):
+    input_size = 1021
+
     if model_str == "MLP":
-        # build model
-        input_size = 1021
+        # build MLP
         hidden_sizes = param_combo['hidden_sizes']
         dropout = param_combo['dropout']
         output_size = len(param_combo['metrics_subset'])
         model = sm.MLP(input_size=input_size, output_size=output_size, dropout=dropout, hidden_sizes=hidden_sizes)
-        model.to(device)
+        model = model.to(device)
 
         # build optimizer
         params = model.parameters()
@@ -63,7 +64,7 @@ def build_configuration(model_str, device, param_combo):
         optimizer_func = partial(param_combo['optimizer'])
         optimizer = optimizer_func(params=params, lr=lr)
 
-        # build scheduler and scaler
+        # build scheduler
         scheduler_func = param_combo['scheduler']
         if scheduler_func == optim.lr_scheduler.StepLR:
             scheduler = scheduler_func(optimizer=optimizer, step_size=10, gamma=0.1)
@@ -75,8 +76,40 @@ def build_configuration(model_str, device, param_combo):
             scheduler = scheduler_func(optimizer=optimizer, mode='min', factor=0.1, patience=5)
         else:
             scheduler = scheduler_func(optimizer=optimizer)
-        scaler = torch.GradScaler()
-        return model, optimizer, scheduler, scaler
+    
+    if model_str == "KAN":
+        # build KAN
+        hidden_sizes = param_combo['hidden_sizes']
+        scale_noise = param_combo['scale_noise']
+        spline_order = param_combo['spline_order']
+        output_size = len(param_combo['metrics_subset'])
+        model = sm.KAN(input_size, output_size, hidden_sizes, scale_noise=scale_noise, spline_order=spline_order).to(device)
+
+        # build optimizer
+        params = model.parameters()
+        lr = param_combo['lr']
+        optimizer_func = partial(param_combo['optimizer'])
+        optimizer = optimizer_func(params=params, lr=lr)
+
+        # build scheduler
+        scheduler_func = param_combo['scheduler']
+        if scheduler_func == optim.lr_scheduler.StepLR:
+            scheduler = scheduler_func(optimizer=optimizer, step_size=10, gamma=0.1)
+        elif scheduler_func == optim.lr_scheduler.MultiStepLR:
+            scheduler = scheduler_func(optimizer=optimizer, milestones=[10, 20], gamma=0.1)
+        elif scheduler_func == optim.lr_scheduler.CosineAnnealingLR:
+            scheduler = scheduler_func(optimizer=optimizer, T_max=10)
+        elif scheduler_func == optim.lr_scheduler.ReduceLROnPlateau:
+            scheduler = scheduler_func(optimizer=optimizer, mode='min', factor=0.1, patience=5)
+        elif scheduler_func == optim.lr_scheduler.CosineAnnealingWarmRestarts:
+            scheduler = scheduler_func(optimizer=optimizer, T_0=10, T_mult=2)
+        elif scheduler_func == optim.lr_scheduler.ExponentialLR:
+            scheduler = scheduler_func(optimizer=optimizer, gamma=0.95)
+        else:
+            scheduler = scheduler_func(optimizer=optimizer)
+
+    scaler = torch.GradScaler()
+    return model, optimizer, scheduler, scaler
 
 
 def store_data(model_str, combo_num, metrics_df):
@@ -109,49 +142,60 @@ def create_metrics_df():
 def cat_results(model_str='MLP'):
     search_dir = f'/gv1/projects/GRIP_Precog_Opt/surrogates/{model_str}/gs_combos'
     master_df = create_metrics_df()
-    for i in range(3840):
+    for i in range(4320):
         metrics_path = search_dir + f'/combo{i}_metrics.csv'
-        metrics_df = pd.read_csv(metrics_path)
+        try:
+            metrics_df = pd.read_csv(metrics_path)
+        except:
+            continue
         master_df = pd.concat([master_df, metrics_df], ignore_index=True)
     out_path = f'/gv1/projects/GRIP_Precog_Opt/surrogates/{model_str}/gs_master.csv'
     master_df.to_csv(out_path, index=False)
     return None
 
-cat_results()
 
-# if __name__ == '__main__':
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument('-cn', '--combo_num', type=int, required=True, default=None)
-#     parser.add_argument('-cp', '--cfg_path', type=str, required=False, default='/home/tthakur9/precog-opt-grip/conf.toml')
-#     args = parser.parse_args()
-#     combo_num = args.combo_num
-#     cfg_path = args.cfg_path
-#     all_cfg = toml.load(cfg_path)
-#     cfg = all_cfg['surrogate']
-#     models = cfg['models']
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-cn', '--combo_num', type=int, required=True, default=None)
+    parser.add_argument('-cp', '--cfg_path', type=str, required=False, default='/home/tthakur9/precog-opt-grip/conf.toml')
+    args = parser.parse_args()
+    combo_num = args.combo_num
+    cfg_path = args.cfg_path
+    all_cfg = toml.load(cfg_path)
+    cfg = all_cfg['surrogate']
+    model_str = "KAN"
+    if model_str == "MLP":
+        # define MLP-unique parameter grid
+        param_grid = {
+                    'dropout': [0.0, 0.2, 0.4, 0.6],
+                    'hidden_sizes': [[512, 256], [1024, 512], [2048, 1024, 512]],
+                    'optimizer': [optim.SGD, optim.Adam, optim.RMSprop, optim.Adagrad],
+                    'lr': [0.0001, 0.001, 0.01, 0.1],
+                    'scheduler': [lr.StepLR, lr.MultiStepLR, lr.CosineAnnealingLR, lr.ReduceLROnPlateau],
+                    'metrics_subset': [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], [0, 4, 11], [11], [4], [0]]
+        }
 
-#     for model_str in models:
-#         if model_str == "MLP":
-#             # define MLP-unique parameter grid
-#             param_grid = {
-#                         'dropout': [0.0, 0.2, 0.4, 0.6],
-#                         'hidden_sizes': [[512, 256], [1024, 512], [2048, 1024, 512]],
-#                         'optimizer': [optim.SGD, optim.Adam, optim.RMSprop, optim.Adagrad],
-#                         'lr': [0.0001, 0.001, 0.01, 0.1],
-#                         'scheduler': [lr.StepLR, lr.MultiStepLR, lr.CosineAnnealingLR, lr.ReduceLROnPlateau],
-#                         'metrics_subset': [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], [0, 4, 11], [11], [4], [0]]
-#             }
+    elif model_str == "KAN":
+        # define unique KAN param grid
+        param_grid = {
+                    'hidden_sizes': [[512, 256], [2048, 512], [2048, 1024, 512], [2048]],
+                    'optimizer': [optim.SGD, optim.RMSprop, optim.Adadelta, optim.AdamW],
+                    'lr': [0.001, 0.01, 0.1],
+                    'scheduler': [lr.StepLR, lr.CosineAnnealingWarmRestarts, lr.ReduceLROnPlateau, lr.ExponentialLR],
+                    'metrics_subset': [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], [0, 4, 11], [11], [4], [0]],
+                    'scale_noise': [0.1, 0.25],
+                    'spline_order': [2, 3, 4]
+        }
 
-#             # use grid's keys & values to create a list of dicts for each combo in search space
-#             param_names = param_grid.keys()
-#             param_values = param_grid.values()
-#             combinations = list(itertools.product(*param_values))
-#             combinations_dicts = [dict(zip(param_names, combo)) for combo in combinations]
+    # use grid's keys & values to create a list of dicts for each combo in search space
+    param_names = param_grid.keys()
+    param_values = param_grid.values()
+    combinations = list(itertools.product(*param_values))
+    combinations_dicts = [dict(zip(param_names, combo)) for combo in combinations]
 
-#             # run train/val engine with specific parameter combination
-#             if combo_num is not None and combo_num < len(combinations_dicts):
-#                 combo = combinations_dicts[combo_num]
-#                 engine(cfg=cfg, model_str=model_str, param_combo=combo, combo_num=combo_num)
-#             else:
-#                 print(f'No more {model_str} parameter combinations to try.')
-#                 break
+    # run train/val engine with specific parameter combination
+    if combo_num is not None and combo_num < len(combinations_dicts):
+        combo = combinations_dicts[combo_num]
+        engine(cfg=cfg, model_str=model_str, param_combo=combo, combo_num=combo_num)
+    else:
+        print(f'No more {model_str} parameter combinations to try.')
