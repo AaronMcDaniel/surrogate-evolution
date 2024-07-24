571890bdeed314204a5b78b4b2de2ca0a37ead41
Kulkarni <vkulkarni46@ice-login2.icehammer.gtri.org>
2024-07-24 10:50:44 -0400

    Use mix of previous gen data for obtaining larger val set

--- trusts_over_generation.py
+++ trusts_over_generation.py
@@ -6,7 +6,7 @@ import pickle
 import pandas as pd
 import toml
 from surrogate import Surrogate
-from surrogate_dataset import SurrogateDataset, build_dataset, merge_dfs_to_dataset
+from surrogate_dataset import build_dataset
 from surrogate_eval import engine
 
 NUM_GENERATIONS = 20
@@ -32,7 +32,7 @@ for i in range(1, NUM_GENERATIONS+1):
     elif i == 2:
         print('Trained on a portion of generation 1. Validated on a portion of generation 1')
     else:
-        print(f'Trained on generations upto and including {i-2}. Validated on generation {i-1}')
+        print(f'Trained on generations upto and including {i-2}. Validated on portion of generations upto and including generation {i-2} and generation {i-1}')
     
     print('------------------------------------')
     if i == NUM_GENERATIONS:
@@ -44,10 +44,12 @@ for i in range(1, NUM_GENERATIONS+1):
         train_df = pd.read_pickle(f'{DATASETS_FOLDER}/train_dataset.pkl')
         val_df = pd.read_pickle(f'{DATASETS_FOLDER}/val_dataset.pkl')
     else:
-        build_dataset(f'{RUN_FOLDER}/out.csv', RUN_FOLDER, DATASETS_FOLDER, val_ratio=0, include_only=seen_gens)
+        build_dataset(f'{RUN_FOLDER}/out.csv', RUN_FOLDER, DATASETS_FOLDER, val_ratio=0.2, include_only=seen_gens)
         train_df = pd.read_pickle(f'{DATASETS_FOLDER}/train_dataset.pkl')
+        prev_val_df = pd.read_pickle(f'{DATASETS_FOLDER}/val_dataset.pkl')
         build_dataset(f'{RUN_FOLDER}/out.csv', RUN_FOLDER, DATASETS_FOLDER, val_ratio=1, include_only=[i])
-        val_df = pd.read_pickle(f'{DATASETS_FOLDER}/val_dataset.pkl')
+        curr_val_df = pd.read_pickle(f'{DATASETS_FOLDER}/val_dataset.pkl')
+        val_df = pd.concat([prev_val_df, curr_val_df])
     
     calc_pool = surrogate.get_individuals_from_file(f'{RUN_FOLDER}/out.csv', hashes=val_df['hash'].to_list())
     seen_gens.append(i)
@@ -56,7 +58,7 @@ for i in range(1, NUM_GENERATIONS+1):
     model_dicts = [surrogate.models[i] for i in SUB_SURROGATES]
     for model_dict in model_dicts:
         print(f'    Training {model_dict['name']}...')
-        metrics, genome_scaler = engine(surrogate_config, model_dict, train_df, val_df)    
+        metrics, best_epoch_num, genome_scaler = engine(surrogate_config, model_dict, train_df, val_df)    
     
     print('Getting trust score...')
     surrogate.trust = surrogate.calc_ensemble_trust(SUB_SURROGATES, genome_scaler, calc_pool)
