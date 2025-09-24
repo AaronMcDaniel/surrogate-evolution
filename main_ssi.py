c3eb98fd5dc04639256c539330df38f2d5c6e061
Pranav Somu <pranavcsomu@gmail.com>
2025-09-24 18:04:01 -0400

    Add retry mechanism for failed SSI iteration

--- main_ssi.py
+++ main_ssi.py
@@ -13,6 +13,10 @@ import copy
 import hashlib
 import math
 import torch
+import pandas
+
+pandas.set_option('display.max_colwidth', None)
+pandas.set_option('display.max_columns', None)
 
 parser = argparse.ArgumentParser()
     
@@ -52,11 +56,33 @@ def print_random_state_fingerprint(random_state, np_random_state):
     print("NP RANDOM HASH", np_hash)
 
 SEED = 93
-random.seed(SEED)
-np.random.seed(SEED)
-torch.manual_seed(SEED)
+
+def setSeed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+setSeed(SEED)
 
 REMOVE_PARTITIONED_POPULATION_ABLATION = False
+
+
+def perform_ssi(elites):
+    selection_pool = copy.deepcopy(elites + GaPipeline.current_deap_pop)
+    selection_pool = {GaPipeline.get_hash_public(str(x)):x for x in selection_pool}
+    unsustainable_pop = GaPipeline.simulated_surrogate_injection_new(selection_pool)
+    if not REMOVE_PARTITIONED_POPULATION_ABLATION:
+        remove_hashes = set()
+        for hash in selection_pool:
+            if hash in unsustainable_pop:
+                remove_hashes.add(hash)
+        for hash in remove_hashes:
+            del selection_pool[hash]
+        k = math.ceil(GaPipeline.population_size*(1-GaPipeline.ssi_population_percentage))
+        retained_pop = tools.selNSGA2(list(selection_pool.values()), k=k)
+        retained_pop = GaPipeline.overpopulate(retained_pop, custom_pop_size=k)
+        unsustainable_pop.update(retained_pop)
+    return unsustainable_pop
+
 
 GaPipeline = Pipeline(output_dir, config_dir, force_flag, clean)
 GaPipeline.initialize(seed_file)
@@ -73,10 +99,8 @@ while GaPipeline.gen_count <= num_gen:
         # just train the surrogate, don't evaluate generation on resume
         all_subsurrogate_metrics = GaPipeline.prepare_surrogate()
 
-    random.seed(int(SEED*(GaPipeline.gen_count+1)))
-    np.random.seed(int(SEED*(GaPipeline.gen_count+1)))
-    torch.manual_seed(int(SEED*(GaPipeline.gen_count+1)))
-    
+    setSeed(int(SEED*(GaPipeline.gen_count+1)))
+
     if not GaPipeline.attempt_resume:
         elites = GaPipeline.update_elite_pool() # elites are selected from existing elite pool and current pop
     else :
@@ -88,22 +112,21 @@ while GaPipeline.gen_count <= num_gen:
     print_random_state_fingerprint(random.getstate(), np.random.get_state())
 
     unsustainable_pop = None
+    if all_subsurrogate_metrics is None:
+        print('NO SURROGATE METRICS; SSI WILL NOT RUN IF ATTEMPTED THIS GENERATION', flush=True)
     if ssi and (GaPipeline.gen_count >= ssi_start_gen) and ((GaPipeline.gen_count - ssi_start_gen) % ssi_freq == 0) and all_subsurrogate_metrics is not None:
         # returns pop dict
-        selection_pool = copy.deepcopy(elites + GaPipeline.current_deap_pop)
-        selection_pool = {GaPipeline.get_hash_public(str(x)):x for x in selection_pool}
-        unsustainable_pop = GaPipeline.simulated_surrogate_injection_new(selection_pool)
-        if not REMOVE_PARTITIONED_POPULATION_ABLATION:
-            remove_hashes = set()
-            for hash in selection_pool:
-                if hash in unsustainable_pop:
-                    remove_hashes.add(hash)
-            for hash in remove_hashes:
-                del selection_pool[hash]
-            k = math.ceil(GaPipeline.population_size*(1-GaPipeline.ssi_population_percentage))
-            retained_pop = tools.selNSGA2(list(selection_pool.values()), k=k)
-            retained_pop = GaPipeline.overpopulate(retained_pop, custom_pop_size=k)
-            unsustainable_pop.update(retained_pop)
+        retry_counter = 0
+        max_retries = 5
+        while retry_counter < max_retries:
+            try:
+                unsustainable_pop = perform_ssi(elites)
+                break  # If successful, exit the loop
+            except Exception as e:
+                retry_counter += 1
+                setSeed(int(SEED*(GaPipeline.gen_count+1)) + retry_counter)
+                print(f"SSI attempt {retry_counter} failed with error: {e}. Retrying...")
+        setSeed(int(SEED*(GaPipeline.gen_count+1)))
     else:
         selected_parents = GaPipeline.select_parents(elites + GaPipeline.current_deap_pop) 
         unsustainable_pop = GaPipeline.overpopulate(selected_parents)
